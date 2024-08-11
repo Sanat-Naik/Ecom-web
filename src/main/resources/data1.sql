45912b2bd835797b339051c268bc95836d5e2536
Sanat-Naik <bgsanat2001@gmail.com>
2024-08-12 02:06:58 +0530

    initial  back-end commit

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(name,description,brand,price,category,release_date,available,quantity)VALUES
+('Tata','good one','tata Motor',75,'car','2022-01-15',true,50),
+ ('Maruthi','good one and best one','Maruthi Motor',80,'car','2023-01-15',true,20),
+('Mahindra','good one and best look','Mahindra Motor',85,'car','2024-01-15',false,10);
